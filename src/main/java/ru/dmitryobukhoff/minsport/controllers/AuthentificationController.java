1de34763074f7e4399b803041c03d5063d9e2a4a
Dmitry Obukhov <obuhkov-da04@yandex.ru>
2023-04-27 18:03:44 +0300

    commit

--- src/main/java/ru/dmitryobukhoff/minsport/controllers/AuthentificationController.java
+++ src/main/java/ru/dmitryobukhoff/minsport/controllers/AuthentificationController.java
@@ -1,21 +1,44 @@
 package ru.dmitryobukhoff.minsport.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.dmitryobukhoff.minsport.models.UsersEntity;
 
 @Controller()
 @RequestMapping("/auth")
 public class AuthentificationController {
+//    @Autowired
+//    @Qualifier(value = "UserDAO")
+//    private UserDAO userDAO;
+
     @GetMapping()
     public String authentificationPage(Model model){
-        //model.addAttribute("user", new User());
+        model.addAttribute("person", new UsersEntity());
         return "auth/registration";
     }
 
     @GetMapping("/exit")
     public String exitPage(){
         return "auth/exit";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid UsersEntity user,
+                         BindingResult bindingResult){
+        if(!bindingResult.hasErrors()){
+            return "auth/registration";
+        }
+        //сохранение в БД
+        return "redirect:/sport";
+
+
     }
 }
